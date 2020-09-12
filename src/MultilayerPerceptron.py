a400ce7d7947c3be9cc8755bc83a0e8ea06055da
Luiz Cartolano <duducartolano@gmail.com>
2020-09-12 19:09:22 -0300

    add multilayer perceptron class

---
+++ src/MultilayerPerceptron.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+
+
+class MultilayerPerceptron(nn.Module):
+    def __init__(self, input_size, layer1_size=400, layer2_size=400,
+                 layer3_size=400, layer4_size=400, output_size=1):
+        """
+
+            :param input_size:
+            :param layer1_size:
+            :param layer2_size:
+            :param layer3_size:
+            :param layer4_size:
+            :param output_size:
+        """
+        super(MultilayerPerceptron, self).__init__()
+
+        self.input_size = input_size
+        self.layer1_size = layer1_size
+        self.layer2_size = layer2_size
+        self.layer3_size = layer3_size
+        self.layer4_size = layer4_size
+        self.output_size = output_size
+
+        # hidden layers
+        self.layer1 = nn.Sequential(
+            nn.Linear(self.input_size, self.layer1_size),
+            nn.LeakyReLU()
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(self.layer1_size, self.layer2_size),
+            nn.LeakyReLU()
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(self.layer2_size, self.layer3_size),
+            nn.LeakyReLU()
+        )
+        self.layer4 = nn.Sequential(
+            nn.Linear(self.layer3_size, self.layer4_size),
+            nn.LeakyReLU()
+        )
+
+        # output layer
+        self.output = nn.Linear(self.layer4_size, self.output_size)
+
+    def forward(self, x):
+        """
+
+            :param x:
+            :return:
+        """
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        output = self.output(x)
+
+        return output
