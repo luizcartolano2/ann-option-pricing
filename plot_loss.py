1d9dec6aafac3e2c50062a7963c48bdcd65c33a8
Luiz Cartolano <duducartolano@gmail.com>
2020-09-13 17:19:35 -0300

    func to plot loss

---
+++ plot_loss.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import re
+
+filename = 'records/loss_20-09-13.txt'
+
+with open(filename) as f:
+    lines = f.readlines()
+
+epochs = []
+train_loss = []
+valid_loss = []
+
+print()
+epoch = 0
+for i, line in enumerate(lines):
+    if 'Epoch' in line:
+        epoch += 1
+        epochs.append(epoch)
+        print(f'Epoch: {epoch}')
+    elif 'train' in line:
+        temp1 = line.find(':')
+        temp2 = line[temp1:].find(',')
+        loss = float(line[temp1+1: temp1+temp2])
+        train_loss.append(loss)
+
+        print(f'\tTrain Loss: {loss}')
+    else:
+        temp1 = line.find(':')
+        temp2 = line[temp1:].find(',')
+        loss = float(line[temp1 + 1: temp1 + temp2])
+        valid_loss.append(loss)
+
+        print(f'\tValid Loss: {loss}')
+
+
+plt.plot(epochs, train_loss, label='Train Loss')
+plt.plot(epochs, valid_loss, label='Valid Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss (MSE)')
+plt.legend()
+
+plt.savefig('figures/train-mlp-impvol.png')
